7a6450a1fa8047eff19a2e979871120b9436d893
MoraMaz <lestermaz.96@gmail.com>
2017-12-22 18:04:10 -0600

    Árbol y Lista de canciones
    Creado el árbol binario de búsqueda y la lista circular de canciones

---
+++ Python/ArbolBinario.py
@@ -0,0 +1,53 @@
+import ListaCanciones
+
+class NodoBinario(object):
+	def __init__(self, album):
+		self.album = album
+		self.lista = None
+		self.izquierda = ArbolBinario()
+		self.derecha = ArbolBinario()
+
+class ArbolBinario(object):
+	def __init__(self):
+		self.nodo = None
+
+	def insertar(self, album):
+		nuevo = NodoBinario(album)
+		if self.nodo == None:
+			self.nodo = nuevo
+		elif self.nodo.album > album:
+			self.nodo.izquierda.insertar(album)
+		elif self.nodo.album < album:
+			self.nodo.derecha.insertar(album)
+		else:
+			print "El archivo ya existe."
+
+	def eliminar(self, album):
+		if self.nodo != None:
+			if self.nodo.album == album:
+				if self.nodo.izquierda.nodo == None and self.nodo.derecha.nodo == None:
+					self.nodo = None
+				elif self.nodo.izquierda.nodo != None and self.nodo.derecha.nodo == None:
+					self.nodo = self.nodo.izquierda.nodo
+				elif self.nodo.izquierda.nodo == None and self.nodo.derecha.nodo != None:
+					self.nodo = self.nodo.derecha.nodo
+				else:
+					predecesor = self.nodo.izquierda.nodo
+					while predecesor.derecha.nodo != None:
+						predecesor = predecesor.derecha.nodo
+					self.nodo.album = predecesor.album
+					self.nodo.izquierda.eliminar(predecesor.album)
+			elif self.nodo.album > album:
+				self.nodo.izquierda.eliminar(album)
+			else:
+				self.nodo.derecha.eliminar(album)
+
+arbol = ArbolBinario()
+arbol.insertar(10)
+arbol.insertar(5)
+arbol.insertar(15)
+
+nodo = arbol.nodo
+print nodo.izquierda.nodo.album
+print nodo.album
+print nodo.derecha.nodo.album
